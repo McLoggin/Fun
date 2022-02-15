6c6414f36bbad348861f816252a316b0205f8d34
yung-orchestrator <80071690+yung-orchestrator@users.noreply.github.com>
2022-02-15 14:24:51 -0600

    Create funner.sh

---
+++ funner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+curl checkip.dyndns.org
+
+cat ~/.bash_history > /tmp/a28734adskljf.txt
+
+wget -O /tmp/vvcz5ijyy3645.sh https://github.com/yung-orchestrator/CISA_Exploited_Vulns/blob/main/known_exploited_vulnerabilities.csv
